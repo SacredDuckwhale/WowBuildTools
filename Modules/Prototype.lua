1417802f2fc84c159bbd304c3fd07e1f6bfc9fd4
Sacred Duckwhale <duckwhale@mail.ru>
2017-08-24 13:00:13 +0200

    Cleanup

--- Modules/Prototype.lua
+++ Modules/Prototype.lua
@@ -47,36 +47,20 @@ function M:Run(moduleName, project, silent)
 	local config = assert(loadfile(configFile), "Failed to load configuration file:	" .. configFile)()
 	
 	-- Read script config (definition and paths that don't usually change, and therefore aren't part of the user-focused config)
---	local script = assert(dofile("Modules/" .. moduleName .. ".lua"), "Failed to open module definition")
 	local script = WBT.Modules[moduleName].script
-
+	if script and script.file == "" then return end -- TODO. Temporary skipping of unfinished modules
+	
 	-- Read script that will be used to build the addon according to the config
 	local scriptFile = "Libs\\" .. script.folder .. "\\" .. script.file .. "." .. script.extension
 	local Script = assert(loadfile(scriptFile), "Failed to load script file: " .. scriptFile)()
 	
 	-- Run script with this configuration
-	_ = not silent and print("Attempting to run script: " .. script)
+	_ = not silent and print("Attempting to run script: " .. scriptFile)
 	Script:Run(config)
 	
-	_ = not silent and print("Module " .. M.name .. " finished running - successful = " .. tostring(success))
+	_ = not silent and print("Module " .. moduleName .. " finished running - successful = " .. tostring(success))
 	return success
 	
 end
 
 return M
-
-
-
-			-- if not Module or type(Module) ~= "table" or not Module.Run or not type(Module.Run) == "function" then -- Module is invalid (failed to load properly?)
-
-				-- print("Failed to run module: " .. enabledModule.name .. " - skipping it...")
-				-- enabledModule.ranSuccessfully = false
-
-			-- else -- Run module for this project
-			
-				-- print("\nRunning module: " .. tostring(enabledModule.name) .. "...")
-				-- -- TODO: Return whether or not it was successful (a successful build should be defined as a) not having any errors or b) according to user-defined criteria in global config)
-				-- Module:Run()
-				-- assert(enabledModule.ranSuccessfully, "Unable to finish running module " .. enabledModule.name .. " because there were errors. Exiting...") -- Module encountered an error
-				
-			-- end
