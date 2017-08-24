25bef015e5866729d64ed5fa4a7a440cd8e0134a
Sacred Duckwhale <duckwhale@mail.ru>
2017-08-24 12:48:15 +0200

    Added definition for Locales module

--- Modules/Locales.lua
+++ Modules/Locales.lua
@@ -1,34 +1,11 @@
--- TODO: This is just a prototype module
-M = {}
+local Module = {}
 
-M.name = "Locales"
-M.libsFolder = "scrapewhale"
-M.libsFile = "scrapewhale"
+script = {}
+----------------------------------------------------------------------------------------------------------------------
+script.folder = "scrapewhale"
+script.file = "scrapewhale"
+script.extension = "lua"
+----------------------------------------------------------------------------------------------------------------------
+Module.script = script
 
-function M:Run(silent)
-	
-	print("Module " .. M.name .. " is running...")
-	local success = true
-	
-	if not silent then
-		print("Module is running in verbose mode")
-	end
-	
-	-- Read config for this module
-	local configFile = "Config/" .. M.name .. ".lua"
-	local config = assert(dofile(configFile), "Failed to load configuration file: " .. configFile)
-	
-	-- Read script that will be used to build the addon according to the config
-	local script = "Libs\\" .. M.libsFolder .. "\\" .. M.libsFile .. ".lua"
-	print("Attempting to run script: " .. script)
-	local Script = assert(dofile(script), "Failed to load script file: " .. script)
---dump(Module)
-	-- Run script with this configuration
-	Script:Run(config)
-	
-	print("Module " .. M.name .. " finished running - successful = " .. tostring(success))
-	return success
-	
-end
-
-return M
+return Module
