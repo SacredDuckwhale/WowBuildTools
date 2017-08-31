53c8c09329ca42941601e6adb90d9afc1e8d6ea5
Sacred Duckwhale <duckwhale@mail.ru>
2017-08-31 11:04:12 +0200

    Added initial version of StaticAnalysis module (luacheck wrapper)

--- Config/StaticAnalysis.lua
+++ Config/StaticAnalysis.lua
@@ -14,9 +14,14 @@ ignoreFolders = { -- Folders that should not be checked
 	
 }
 
-quietMode = true -- Only display errors
-filter = "011" -- Apply this filter
- 
+--quietMode = true -- Only display errors TODO: Doesn't work with module (only CLI?)
+detailedReports = true -- Show detailed report messages (as opposed to just a summary) if things have gone wrong
+strictMode = true -- Stop build if warnings occured, and not just fatals/errors
+
+filter = { "011" } -- Apply these filters
+configPath = "F://GitHub/WowBuildTools" -- .luacheckrc file path
+cacheResults = true -- Improves run time by only checking files that changed since the last time they were checked (stores cache in .luacheckcache)
+
 ----------------------------------------------------------------------------------------------------------------------
 -- Stop editing here
 ----------------------------------------------------------------------------------------------------------------------
--- Modules/StaticAnalysis.lua
+++ Modules/StaticAnalysis.lua
@@ -14,10 +14,13 @@ function script:GetArgs(project, config, silent)
 
 	local args = {}
 
-	args.quiet = config.quietMode or true
-	args.filter = config.filter or "011"
+	--args.quiet = config.quietMode or true
+	args.only = config.filter or{ "011" }
 	args.ignoredFolders = config.ignoreFolders or {}
-	
+	args.cache = config.cacheResults or true
+	args.config = (config.configPath and config.configPath .. "/") or "."
+	args.detailedReports = config.detailedReport or true
+	args.strictMode = config.strictMode or true
 	args.startDir = project.root or ".."
 	
 	return args
